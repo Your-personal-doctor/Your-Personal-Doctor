d44eac44cfcb5709dfec1875594a8607fd1b35f8
java <34083350+Zhuruife@users.noreply.github.com>
2018-10-18 17:42:56 -0400

    Update Massage.java

--- web/src/web/Massage.java
+++ web/src/web/Massage.java
@@ -1,9 +1,24 @@
 package web;
 
 /**
- *
+ * data: 9/25/2018
  * @author Ruifeng zhu
  */
-public class Massage {
-    
+public class Message {
+
+    private final int _thismessage;
+
+
+    public Message(int message) {
+        this._thismessage = message;
+    }
+
+    public int setMessage() {
+        return this._thismessage;
+    }
+
+    public int getMessage() {
+        return this._thismessage;
+    }
+
 }
